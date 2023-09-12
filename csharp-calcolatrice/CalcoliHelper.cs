ddbd046d38354bc2be3448bf7a66fc195037b995
Blaser993 <corradodepinto.design@gmail.com>
2023-09-12 15:02:18 +0200

    Calcolatrice semplice finita

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal class CalcoliHelper
+    {
+
+        public static int SommaInteri(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double SommaDecimali(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int DifferenzaInteri(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double DifferenzaDecimali(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int ProdottoInteri(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double ProdottoDecimali(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int ValAssolutoInt(int num)
+        {
+            if (num == 0)
+            {
+                return 0;
+            }
+            else if (num >= 1)
+            {
+                return num;
+            }
+            else
+            {
+                return num * -1;
+            }
+
+        }
+
+        public static double ValAssolutoDecimale(double num)
+        {
+            if (num == 0)
+            {
+                return 0;
+            }
+            else if (num >= 1)
+            {
+                return num;
+            }
+            else
+            {
+                return num * -1;
+            }
+        }
+
+        public static int MinInt(int num1, int num2)
+        {
+            switch (num1 - num2)
+            {
+                case 0:
+                    return num1;
+                    break;
+                case < 0:
+                    return num1;
+                    break;
+                default:
+                    return num2;
+                    break;
+            }
+            
+        }
+
+        public static double MinDec(double num1, double num2)
+        {
+            switch (num1 - num2)
+            {
+                case 0:
+                    return num1;
+                    break;
+                case < 0:
+                    return num1;
+                    break;
+                default:
+                    return num2;
+                    break;
+            }
+
+        }
+
+        public static int MaxInt(int num1, int num2)
+        {
+            switch (num1 - num2)
+            {
+                case 0:
+                    return num1;
+                    break;
+                case < 0:
+                    return num2;
+                    break;
+                default:
+                    return num1;
+                    break;
+            }
+        }
+
+        public static double MaxDec(double num1, double num2)
+        {
+            switch (num1 - num2)
+            {
+                case 0:
+                    return num1;
+                    break;
+                case < 0:
+                    return num2;
+                    break;
+                default:
+                    return num1;
+                    break;
+            }
+
+        }
+
+    }
+}
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using csharp_calcolatrice;
+
 Console.WriteLine("Hello, World!");
+
+/*
+Come visto a lezione, strutturare la classe in modo che non possa essere istanziata e che presenti i seguenti metodi static:
+Somma di due numeri interi
+Somma di due numeri double
+Differenza tra due numeri interi
+Differenza tra due numeri double
+Moltiplicazione di due numeri interi
+Moltiplicazione di due numeri double
+Valore assoluto di un numero intero
+Valore assoluto di un numero double
+Minimo tra due numeri interi
+Minimo tra due numeri double
+Massimo tra due numeri interi
+Massimo tra due numeri double
+*/
+
+int num = -567;
+int primo = 43;
+int secondo = -42;
+
+double terzo = 34.6;
+double quarto = 34.5;
+
+
+int resultInt = CalcoliHelper.MaxInt(primo,secondo);
+Console.WriteLine(resultInt);
+
+double resultDec = CalcoliHelper.MaxDec(terzo, quarto);
+Console.WriteLine(resultDec);
